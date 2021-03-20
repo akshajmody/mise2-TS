68ff45acaeaccb5aacb068cf1cb01b60e1921fa9
Akshaj Mody <akshajmody@gmail.com>
2021-03-20 21:19:29 +0900

    add mock api data to use with mock api interceptor

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
 // import React from 'react';
+import { useState } from "react";
+import { useQuery } from "react-query";
+//Material UI imports
+import Drawer from "@material-ui/core/Drawer";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import Badge from "@material-ui/core/Badge";
+//Styles
+import { Wrapper } from "./App.styles";
+//Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  //OUTER AWAIT IS FOR CONVERSION TO JSON - conversion to JSON is also async
+  //INNER AWAIT IS FOR API CALL ITSELF
+  await (await fetch("https://fakestoreapi.com/products")).json();
 
 const App = () => {
-  return <div className="App">START</div>;
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
+  );
+  console.log(data);
+
+  const getTotalItems = () => null;
+  const handleAddToCart = () => null;
+  const handleRemoveFromCart = () => null;
+
+  return <div className="App">start</div>;
 };
 
 export default App;
